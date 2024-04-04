f3b6b5b5ff598c3a879fb3a1626ac7d98180ede8
Serg <serg@mail.ru>
2024-04-04 13:07:18 +0300

    Create Client and Bank

--- BankService.cpp
+++ BankService.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <string>
+#define random(min, max) min + rand() % (max - min + 1)
 
 using namespace std;
 
@@ -18,11 +20,16 @@ private:
 
     Client() {};
 
-    Client(string firstName, string lastName, string bankAccount) {
+    Client(string firstName, string lastName, 
+        unsigned short age, 
+        string bankAccount) {
+
         this->id = lastId++;
         this->firstName = firstName;
         this->lastName = lastName;
+        this->age = age;
         this->bankAccount = bankAccount;
+
     }
 
     void setFirstName(string fName) {
@@ -76,11 +83,72 @@ private:
 int Client::lastId = 1;
 
 class Bank {
+private:
+    string name;
+    Client* clients = new Client[0];
+    int size = 0;
+public:
+    Bank(string name) {
+        this->name = name;
+    }
+    
+    ~Bank() {
+        delete[] clients;
+    }
+
+    void createClient(string firstName,
+                      string lastName,
+                      unsigned short age) {
+        Client* buf = new Client[size + 1];
+        for (int i = 0; i < size; i++)
+        {
+            buf[i] = clients[i];
+        }
+        delete[] clients;
+        clients = buf;
+        clients[size] = Client(firstName, lastName, age, createBankAccount());
+        size++;
+    }
+
+    void showClients() {
+        for (int i = 0; i < size; i++)
+        {
+            clients[i].showInfo();
+        }
+    }
+
+private:
+    string createBankAccount() {
+        string bankAccount;
+        do {
+            bankAccount = "";
+            bankAccount += to_string(random(1000, 9999));
+            bankAccount += to_string(random(1000, 9999));
+        } while (checkBankAccount(bankAccount));
+
+        return bankAccount;
+    }
+
+    bool checkBankAccount(string bankAccount) {
+        for (int i = 0; i < size; i++)
+        {
+            if (clients[i].getBankAccount() == bankAccount) return true;
+        }
+        return false;
+    }
 
 };
 
 
 int main()
 {
+    srand(time(NULL));
+    Bank bank("NYBank");
+    for (int i = 0; i < 200; i++)
+    {
+        bank.createClient("John", "Smith", 25);
+    }
+    bank.showClients();
+
     
 }
